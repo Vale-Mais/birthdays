ca0beb062a28658ff65c4586d7ea78b5fef04540
giougolini <ugolini.gio@gmail.com>
2020-12-17 00:42:21 +0100

    added test suite with 4 test cases

---
+++ testing.py
@@ -0,0 +1,44 @@
+import unittest
+import checker as c
+import adding as a 
+import pandas as pd
+import csv
+
+class TestName(unittest.TestCase):
+    
+    @classmethod
+    def tearDownClass(cls):
+        db = pd.DataFrame(pd.read_csv('people_birthday.csv'))
+        index = db.loc[db["Person"] == "Albi"].index.values[0]
+        db.drop(db.index[index],inplace=True)
+        db.drop("Unnamed: 0", axis=1, inplace=True)
+        db.to_csv("people_birthday.csv")
+        
+    def setUp(self): 
+        self.correct_p = "Valeria"
+        self.db = pd.DataFrame(pd.read_csv('people_birthday.csv'))
+        self.new_p = "Daniel"
+    
+    def test_check_person(self):
+        self.assertTrue(c.check_person(self.correct_p))
+        self.assertFalse(c.check_person(self.new_p))
+        
+    def test_return_birthday(self):
+        result1 = print('{}\'s birthday is {}.'.format(self.correct_p, self.db["Birthday"].loc[self.db["Person"].str.lower()==self.correct_p.lower()].values[0]))
+        self.assertEqual(c.return_birthday(self.correct_p),result1)
+        result2 = print('Sadly, we don\'t have {}\'s birthday.'.format(self.new_p))
+        self.assertEqual(c.return_birthday(self.new_p),result2)
+    
+    def test_print_birthdays(self):
+        result = print(self.db)
+        self.assertEqual(c.print_birthdays(),result)  
+    
+    def test_add_person(self):
+        res1 = print("sorry, but " + self.correct_p + " is already present in the database, thank you anyway")
+        self.assertEqual(a.add_person(self.correct_p),res1)
+        res2 = print("Thank you for your contribution!")
+        self.assertEqual(a.add_person("Albi"),res2)
+        
+        
+if __name__ == "__main__":
+    unittest.main()
